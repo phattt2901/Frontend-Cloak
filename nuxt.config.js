ab8f579ca899702628267f2e5cafe91538359aa3
phattt2901 <phattt2901@gmail.com>
2023-04-17 00:11:43 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   routeRules: {
     // Use client-side rendering for all routes
-    '/**': { ssr: false },
+    '/**': { ssr: true },
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
